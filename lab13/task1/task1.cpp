8ec55ba949752b30cfb1f8ac162f016915217ea6
Voronezhsky PA <pvoronezhsky@gmail.com>
2019-12-16 18:37:07 +0300

    Fix Task1

--- lab13/task1/task1.cpp
+++ lab13/task1/task1.cpp
@@ -12,28 +12,21 @@ using namespace std;
 
 static int w = 800, h = 600;
 
-
 #define INDEX_BUFFER	0
 #define POS_VB			1
 #define NORMAL_VB		2
-#define TEXCOORD_VB		3
 
 GLuint m_VAO;
-GLuint m_Buffers[4];
+GLuint m_Buffers[3];
 
-objl::Loader ldr;
+objl::Loader loader;
 
 GLuint Program;
 
 GLint Attrib_vertex;
-GLint Attrib_vertex1;
 GLint Attrib_vertex2;
-
-GLint Unif_light;
-
 GLint Unif_affine;
 
-GLuint texture;
 
 float colors[8][4] = {
 	{ 1.0, 1.0, 1.0, 1.0 },
@@ -47,15 +40,6 @@ float colors[8][4] = {
 };
 
 float affineMatr[9] = { 1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0 };
-
-struct vertex
-{
-	GLfloat x;
-	GLfloat y;
-	GLfloat z;
-	GLfloat tx;
-	GLfloat ty;
-};
 
 void Init(void) {
 	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
@@ -83,21 +67,12 @@ std::string readShader(std::string filename)
 
 void shaderLog(unsigned int shader)
 {
-	int infologLen = 0;
-	int charsWritten = 0;
-	char* infoLog;
-	glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &infologLen);
-	if (infologLen > 1)
-	{
-		infoLog = new char[infologLen];
-		if (infoLog == NULL)
-		{
-			std::cout << "ERROR: Could not allocate InfoLog buffer\n";
-			exit(1);
-		}
-		glGetShaderInfoLog(shader, infologLen, &charsWritten, infoLog);
-		std::cout << "InfoLog: " << infoLog << "\n\n\n";
-		delete[] infoLog;
+	GLint success;
+	glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		GLchar InfoLog[1024];
+		glGetShaderInfoLog(shader, sizeof(InfoLog), NULL, InfoLog);
+		fprintf(stderr, "Error compiling shader type %d: '%s'\n", shader, InfoLog);
 	}
 }
 
@@ -107,29 +82,31 @@ void InitShader(void) {
 	const char* vsSource = vs.data();
 	const char* fsSource = fs.data();
 
+	Program = glCreateProgram();
+
 	GLuint vShader, fShader;
 	vShader = glCreateShader(GL_VERTEX_SHADER);
 	glShaderSource(vShader, 1, &vsSource, NULL);
 	glCompileShader(vShader);
-	std::cout << "vertex shader \n";
 	shaderLog(vShader);
 	fShader = glCreateShader(GL_FRAGMENT_SHADER);
 	glShaderSource(fShader, 1, &fsSource, NULL);
 	glCompileShader(fShader);
-	std::cout << "fragment shader \n";
 	shaderLog(fShader);
-	Program = glCreateProgram();
+
 	glAttachShader(Program, vShader);
 	glAttachShader(Program, fShader);
 	glLinkProgram(Program);
 
-	int link_ok;
-	glGetProgramiv(Program, GL_LINK_STATUS, &link_ok);
-	if (!link_ok)
-	{
-		std::cout << "error attach shaders \n";
-		return;
+	GLint success;
+	glGetProgramiv(Program, GL_LINK_STATUS, &success);
+	if (success == 0) {
+		GLchar ErrorLog[1024];
+		glGetProgramInfoLog(Program, sizeof(ErrorLog), NULL, ErrorLog);
+		fprintf(stderr, "Error linking shader program: '%s'\n", ErrorLog);
 	}
+
+	glUseProgram(Program);
 
 	const char* attr_name0 = "coord";
 	Attrib_vertex = glGetAttribLocation(Program, attr_name0);
@@ -139,19 +116,11 @@ void InitShader(void) {
 		return;
 	}
 
-	const char* attr_name1 = "texCoord";
-	Attrib_vertex1 = glGetAttribLocation(Program, attr_name1);
-	if (Attrib_vertex1 == -1)
-	{
-		std::cout << "could not bind uniform " << attr_name1 << std::endl;
-		return;
-	}
-
-	const char* attr_name2 = "clr";
-	Attrib_vertex2 = glGetAttribLocation(Program, attr_name2);
+	const char* attr_name1 = "clr";
+	Attrib_vertex2 = glGetAttribLocation(Program, attr_name1);
 	if (Attrib_vertex2 == -1)
 	{
-		std::cout << "could not bind uniform " << attr_name2 << std::endl;
+		std::cout << "could not bind uniform " << attr_name1 << std::endl;
 		return;
 	}
 
@@ -165,46 +134,32 @@ void InitShader(void) {
 	checkOpenGLerror();
 }
 
-void initTexture()
-{
-	int width, height;
-	unsigned char* image = SOIL_load_image("wall.jpg", &width, &height, 0, SOIL_LOAD_RGB);
-	glGenTextures(1, &texture);
-	glBindTexture(GL_TEXTURE_2D, texture);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
-	glGenerateMipmap(GL_TEXTURE_2D);
-	SOIL_free_image_data(image);
-	glBindTexture(GL_TEXTURE_2D, 0);
-}
-
+//https://triplepointfive.github.io/ogltutor/tutorials/tutorial32.html
 void initVAO()
 {
-	ldr.LoadFile("Bowl.obj");
+	loader.LoadFile("Bowl.obj");
 	glGenVertexArrays(1, &m_VAO);
 	glBindVertexArray(m_VAO);
-	glGenBuffers(4, m_Buffers);
+	glGenBuffers(3, m_Buffers);
 
 	std::vector<objl::Vector3> Positions;
 	std::vector<objl::Vector3> Normals;
-	std::vector<objl::Vector2> TexCoords;
 	std::vector<unsigned int> Indices;
 
-	unsigned int NumVertices = ldr.LoadedMeshes[0].Vertices.size();
-	unsigned int NumIndices = ldr.LoadedMeshes[0].Indices.size();
+	unsigned int NumVertices = loader.LoadedMeshes[0].Vertices.size();
+	unsigned int NumIndices = loader.LoadedMeshes[0].Indices.size();
 
 	Positions.reserve(NumVertices);
 	Normals.reserve(NumVertices);
-	TexCoords.reserve(NumVertices);
 	Indices.reserve(NumIndices);
 
 	for (unsigned int i = 0; i < NumVertices; i++) {
-		Positions.push_back(ldr.LoadedMeshes[0].Vertices[i].Position);
-		Normals.push_back(ldr.LoadedMeshes[0].Vertices[i].Normal);
-		TexCoords.push_back(ldr.LoadedMeshes[0].Vertices[i].TextureCoordinate);
+		Positions.push_back(loader.LoadedMeshes[0].Vertices[i].Position);
+		Normals.push_back(loader.LoadedMeshes[0].Vertices[i].Normal);
 	}
 
 	for (unsigned int i = 0; i < NumIndices; i++) {
-		Indices.push_back(ldr.LoadedMeshes[0].Indices[i]);
+		Indices.push_back(loader.LoadedMeshes[0].Indices[i]);
 	}
 
 	float* COLORS = new float[4 * Positions.size()];
@@ -221,11 +176,6 @@ void initVAO()
 	glEnableVertexAttribArray(Attrib_vertex);
 	glVertexAttribPointer(Attrib_vertex, 3, GL_FLOAT, GL_FALSE, 0, 0);
 
-	glBindBuffer(GL_ARRAY_BUFFER, m_Buffers[TEXCOORD_VB]);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(TexCoords[0]) * TexCoords.size(), &TexCoords[0], GL_STATIC_DRAW);
-	glEnableVertexAttribArray(Attrib_vertex1);
-	glVertexAttribPointer(Attrib_vertex1, 2, GL_FLOAT, GL_FALSE, 0, 0);
-
 	glBindBuffer(GL_ARRAY_BUFFER, m_Buffers[NORMAL_VB]);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 4 * Positions.size(), COLORS, GL_STATIC_DRAW);
 	glEnableVertexAttribArray(Attrib_vertex2);
@@ -234,9 +184,7 @@ void initVAO()
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_Buffers[INDEX_BUFFER]);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(Indices[0]) * Indices.size(), &Indices[0], GL_STATIC_DRAW);
 
-	glBindVertexArray(0);
-
-
+	//glBindVertexArray(0);
 	checkOpenGLerror();
 }
 
@@ -248,22 +196,11 @@ void Reshape(int width, int height) {
 
 void Render() {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-	glUseProgram(Program);
 	glUniformMatrix3fv(Unif_affine, 1, GL_FALSE, affineMatr);
-
-	glBindVertexArray(m_VAO);
-	glBindTexture(GL_TEXTURE_2D, texture);
-	glEnable(GL_TEXTURE_2D);
 	glDrawElements(GL_TRIANGLES,
-		ldr.LoadedMeshes[0].Indices.size(),
+		loader.LoadedMeshes[0].Indices.size(),
 		GL_UNSIGNED_INT,
 		NULL);
-	glDisable(GL_TEXTURE_2D);
-	glBindVertexArray(0);
-
-	glUseProgram(0);
-	checkOpenGLerror();
 	glutSwapBuffers();
 }
 
@@ -340,7 +277,7 @@ int main(int argc, char** argv) {
 	glutInitWindowPosition(100, 100);
 	glutInitWindowSize(w, h);
 	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
-	glutCreateWindow("Task 3");
+	glutCreateWindow("Task 1");
 
 	GLenum glew_status = glewInit();
 	if (GLEW_OK != glew_status)
@@ -357,7 +294,6 @@ int main(int argc, char** argv) {
 
 	Init();
 	InitShader();
-	initTexture();
 	initVAO();
 
 	glutReshapeFunc(Reshape);
